d6dddfab3b87e10d3970d963c44d010cc9e6946c
courtsideKB <kbankhead1992.yahoo.com>
2021-01-04 00:49:21 -0800

    Parsed the JSON data from the OpenWeather API

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ app.get("/", (req, res) => {
     const url = "https://api.openweathermap.org/data/2.5/weather?zip=93536&units=imperial&appid=22d684af311eccad3e178cab9139199d";
 
     https.get(url, (response) => {
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDesc = weatherData.weather[0].description;
+            res.send("The temperature in Lancaster, CA is " + temp + " Fahrenheit with the following conditions: " + weatherDesc);
+        });
     });
-    res.send("Success! Server is up and running...");
 });
 
 app.listen(3000, () => {
